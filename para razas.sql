a0e2bcd95b7c149c38cecabf41d55665985d49a3
Bruno Sánchez Escajadillo <66394573+burnex@users.noreply.github.com>
2023-05-09 19:41:59 -0500

    Delete para razas.sql

--- para razas.sql
+++
@@ -1,7 +0,0 @@
-SELECT Razas.Id AS Id,
-    Animales.Nombre AS Animal,
-    Razas.Nombre AS Raza,
-    Razas.Estado
-FROM Razas
-INNER JOIN Animales ON Animales.Id = Razas.IdAnimal
-WHERE Animales.Nombre = 'Perro';
